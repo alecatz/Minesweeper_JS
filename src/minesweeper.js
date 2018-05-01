289b589693a06b10c5e2f5534cc4f2290a5ac22a
Alessia Peviani <alecatz23@gmail.com>
2018-05-01 21:21:38 +0200

    Added makeBoardBomb function and test to check bomb placement

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,4 +1,4 @@
-//function: create a board of any size
+//function: create player board of any size
 const makePlayerBoard = (rows,cols) => {
   let board = [];
   for (let row = 0; row < rows ; row+=1) {
@@ -6,6 +6,27 @@ const makePlayerBoard = (rows,cols) => {
     for (let col = 0; col < cols ; col+=1) {
       board[row].push(' ');
     }
+  }
+  return board;
+};
+
+//function: create bomb board of any size
+//to fix: number of bombs can potentially be larger than number of cells!
+//to fix: bombs can potentially overlap!
+const makeBombBoard = (rows,cols,bombs) => {
+  let board = [];
+  for (let row = 0; row < rows; row+=1) {
+    board.push([]);
+    for (let col = 0; col < cols; col+=1) {
+      board[row].push(null);
+    }
+  }
+  let placedBombs = 0;
+  while (placedBombs < bombs) {
+    let randomRow = Math.floor(Math.random() * rows);
+    let randomCol = Math.floor(Math.random() * cols);
+    board[randomRow][randomCol] = 'B';
+    placedBombs+=1;
   }
   return board;
 };
@@ -18,10 +39,15 @@ const printBoard = (anyBoard) => {
   }
 };
 
-/*TEST: create empty board, print it, update some cells, print it again
+/*TEST: create empty player board, print it, update some cells, print it again
 let board = makePlayerBoard(3,3);
 printBoard(board);
 board[0][1] = '1';
 board[2][2]= 'B';
+printBoard(board);
+*/
+
+/*TEST: create bomb board and print it
+let board = makeBombBoard(3,3,3);
 printBoard(board);
 */
