78e423da9912100817d65339578b6b00900c16d4
Alessia Peviani <alecatz23@gmail.com>
2018-05-04 18:50:33 +0200

    Moved makeBombBoard (function) to class Board (static method)

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -64,31 +64,34 @@ class Board {
     return board;
   }
 
+  //method (class): create bomb board of any size
+  //to fix: number of bombs can potentially be larger than number of cells!
+  static makeBombBoard(rows,cols,bombs) {
+    let board = [];
+    for (let row = 0; row < rows; row+=1) {
+      board.push([]);
+      for (let col = 0; col < cols; col+=1) {
+        board[row].push(null);
+      }
+    }
+    let placedBombs = 0;
+    while (placedBombs < bombs) {
+      let randomRow = Math.floor(Math.random() * rows);
+      let randomCol = Math.floor(Math.random() * cols);
+      if (board[randomRow][randomCol] !== 'B') {
+        board[randomRow][randomCol] = 'B';
+        placedBombs++;
+      }
+    }
+    return board;
+  }
+
 }
 
 
 
-//function: create bomb board of any size
-//to fix: number of bombs can potentially be larger than number of cells!
-const makeBombBoard = (rows,cols,bombs) => {
-  let board = [];
-  for (let row = 0; row < rows; row+=1) {
-    board.push([]);
-    for (let col = 0; col < cols; col+=1) {
-      board[row].push(null);
-    }
-  }
-  let placedBombs = 0;
-  while (placedBombs < bombs) {
-    let randomRow = Math.floor(Math.random() * rows);
-    let randomCol = Math.floor(Math.random() * cols);
-    if (board[randomRow][randomCol] !== 'B') {
-      board[randomRow][randomCol] = 'B';
-      placedBombs++;
-    }
-  }
-  return board;
-};
+
+
 
 
 
